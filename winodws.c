1bde3bebcf4ee33eb2442e3b3bd5deb9e200b925
DraXu69_ <dr4x@dr4x.life>
2021-08-25 14:32:54 +0200

    guwno compabilty

--- winodws.c
+++ winodws.c
@@ -1,10 +1,21 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include <unistd.h>
 #define DRAX_COCK_SIZE 23
 
 int
 main(int argc, char *argv[])
 {
-	long n = sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE);
+	long n;
+#ifdef _WIN32
+	MEMORYSTATUSEX status;
+	status.dwLength = sizeof status;
+	GlobalMemoryStatusEx(&status);
+	n = status.ullTotalPhys;
+#else
+	n = sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGE_SIZE);
+#endif
 	void *guwno = malloc(n/2);
 	while ( DRAX_COCK_SIZE == 23 ) memset(guwno, 'n', n/2);
 }
